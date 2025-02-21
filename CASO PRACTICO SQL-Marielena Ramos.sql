701575799408ea4e0e00639e6af85d20396fa4ea
MarielenaR <marielramos9902@gmail.com>
2025-02-21 17:20:46 -0600

    Add files via upload

--- CASO PRACTICO SQL-Marielena Ramos.sql
+++ CASO PRACTICO SQL-Marielena Ramos.sql
@@ -47,3 +47,95 @@ FROM order_details;
 SELECT COUNT(DISTINCT order_id) AS orders_between_dates
 FROM order_details
 WHERE order_date BETWEEN '2023-01-01' AND '2023-01-05';
+
+1.- Realizar un left join entre entre order_details y menu_items con el identificador
+item_id(tabla order_details) y menu_item_id(tabla menu_items).
+
+SELECT 
+    o.order_id,
+    o.order_details_id,
+    o.order_date,
+    o.order_time,
+    m.item_name,
+    m.category,
+    m.price
+FROM 
+    order_details o
+LEFT JOIN 
+    menu_items m
+ON 
+    o.item_id = m.menu_item_id;
+--Destacar los artículos más populares
+
+SELECT 
+    mi.item_name,
+    COUNT(od.item_id) AS total_pedidos
+FROM 
+    order_details od
+JOIN 
+    menu_items mi ON od.item_id = mi.menu_item_id
+GROUP BY 
+    mi.item_name
+ORDER BY 
+    total_pedidos DESC
+LIMIT 5;  -- Los 5 artículos más populares son:
+
+--Hamburger
+--Edamame
+--Korean Beef Bowl
+--Cheeseburger
+--French
+
+--Producto mas costoso pero menos consumido
+
+SELECT 
+    mi.item_name,
+    mi.price,
+    COUNT(od.item_id) AS total_pedidos
+FROM 
+    order_details od
+JOIN 
+    menu_items mi ON od.item_id = mi.menu_item_id
+GROUP BY 
+    mi.item_name, mi.price
+ORDER BY 
+    total_pedidos ASC, mi.price DESC
+LIMIT 1;
+
+Chicken Tacos, deberían considerar reemplazarlos en el menu
+
+--Cantidad de Pedidos por categoria
+
+SELECT 
+    mi.category, 
+    COUNT(od.item_id) AS cantidad_pedidos
+FROM 
+    order_details od
+JOIN 
+    menu_items mi ON od.item_id = mi.menu_item_id
+GROUP BY 
+    mi.category
+ORDER BY 
+    cantidad_pedidos DESC;
+
+La comida Asiatica es la más cosumida
+
+--Pero al considerar el ingreso por categoría
+
+SELECT 
+    mi.category, 
+    SUM(mi.price) AS ingresos_totales
+FROM 
+    order_details od
+JOIN 
+    menu_items mi ON od.item_id = mi.menu_item_id
+GROUP BY 
+    mi.category
+ORDER BY 
+    ingresos_totales DESC;
+
+La italiana es la que más ingresos genera
+
+
+
+
